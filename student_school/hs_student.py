3c5d7b8886625c63566859ace7b2774892c9fc9f
Jemesson <jemesson_lima@apple.com>
2017-12-02 20:57:50 -0300

    Create student web interface

--- student_school/hs_student.py
+++ student_school/hs_student.py
@@ -2,10 +2,5 @@ from student_school.student import Student
 
 
 class HighSchoolStudent(Student):
-    school_name = "Decisao"
-
-    def get_school_name(self):
-        return self.school_name
-
     def get_name_capitalize(self):
         return super().get_name_capitalize() + "-HS"
